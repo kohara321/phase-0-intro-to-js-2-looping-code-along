a3da40df8db720d0677799729942c1dbb19d7fc9
Kaegan O'Hara <kohara321@gmail.com>
2022-05-19 15:26:45 -0600

    commit tree a945446f967b2ce52b12ebf040a59d16287728eb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age ++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me.`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i=0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and add a bow!`);
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(names, event) {
+    let newArr = [];
+    for( let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  return newArr;
+};
+
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'surprise'));
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
+
